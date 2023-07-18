6ab36db76b3f1a69b98b76a34b271eaa09c596a4
Joud Khanji <joud.hanji@gmail.com>
2023-07-18 13:54:52 +0300

    c

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// Challenge 1
+let num1 = 31;
+let num2 = 2;
+let multiply = 31 * 2;
 
+// Challenge 2
+let random = Math.ceil(Math.random() * 100);
+
+// Challenge 3
+let num3 = 19;
+let num4 = 5;
+let mod = num3 % num4;
+
+// Challenge 4
+let set = [1, 10, 20, 5];
+let max = Math.max(...set);
 // Write your code here
